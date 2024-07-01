a2a7e39a6803c724d0e206f5f1c11a165ef0c5d7
Taylor Ng <taylormng95@gmail.com>
2024-07-01 08:48:00 -0400

    completes tree traversal

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,41 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    result = id
+    nodes_to_visit = [self.root]
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop()
+      if node['id'] == result:
+        return node
+      else:
+        nodes_to_visit = node['children'] + nodes_to_visit
+    return None
+
+tree = Tree(
+      {
+        'tag_name': 'body',
+        'id': None,
+        'children': [
+          {
+            'tag_name': 'div',
+            'id': 'main',
+            'children': [
+              {
+                'tag_name': 'h1',
+                'id': 'heading',
+                'text_content': 'Title',
+                'children': []
+              },
+              {
+                'tag_name': 'h2',
+                'id': None,
+                'text_content': 'Subitle',
+                'children': []
+              }
+            ]
+          }
+        ]
+      }
+    )
+
+print(tree.get_element_by_id('heading'))
